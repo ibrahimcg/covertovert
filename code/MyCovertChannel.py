06d65e847c68fb7aef0337613346f1a248a5cff9
ibrahimcg <icg67253@gmail.com>
2024-12-24 13:16:15 +0300

    broadcast on ether

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,5 +1,10 @@
 from CovertChannelBase import CovertChannelBase
 
+from scapy.sendrecv import srp, sniff
+from scapy.layers.inet import Ether
+from scapy.layers.l2 import ARP, LLC, SNAP
+from scapy.all import get_if_hwaddr
+
 class MyCovertChannel(CovertChannelBase):
     """
     - You are not allowed to change the file name and class name.
@@ -9,17 +14,51 @@ class MyCovertChannel(CovertChannelBase):
         """
         - You can edit __init__.
         """
-        pass
+        self.message = ""
+
+
     def send(self, log_file_name, parameter1, parameter2):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        -
+        -
         """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
+        sender_mac = get_if_hwaddr("eth0")
+        ether = Ether(src = sender_mac, dst = "ff:ff:ff:ff:ff:ff")
+
+        self.message = self.generate_random_binary_message_with_logging(log_file_name)
+
+        for bit in self.message:
+            llc = LLC(dsap=0x69, ssap=int(bit))
+            packet = ether / llc
+            super().send(packet)
+
+        llc = LLC(dsap=0xff, ssap=int(bit))
+        packet = ether / llc
+        super().send(packet)
+        
         
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        -
+        -
         """
-        self.log_message("", log_file_name)
+        def recv_packet(packet):
+            if packet.haslayer(LLC):
+                if packet[LLC].dsap == 0xff:
+                    raise Exception
+                
+                packet.show()
+                self.message = self.message + str(packet[LLC].ssap)
+
+        try:
+            sniff(prn=recv_packet)
+        except Exception:
+            pass
+
+        chars = [self.message[i:i+8] for i in range(0, len(self.message), 8)]
+        
+        message = ""
+        for char in chars:
+            message = message + self.convert_eight_bits_to_character(char)
+
+        self.log_message(message, log_file_name)
