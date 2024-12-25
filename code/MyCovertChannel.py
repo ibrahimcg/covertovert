d8a112ee646edc514a79b2c27786f634d0ca170b
Barış Berk Kottaş <brkottas1104@gmail.com>
2024-12-25 17:32:21 +0300

    fixed sender

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -28,9 +28,11 @@ class MyCovertChannel(CovertChannelBase):
         self.message = self.generate_random_binary_message_with_logging(log_file_name)
 
         for i in range(0, len(self.message), 8):
-            byte = int(self.message[i:i + 8])
+            byte = self.message[i:i + 8]
+            byte = int(byte, 2)
             byte ^= 0x69
-            byte = str(bin(byte))[2:]
+            byte = str(bin(byte))[2:].zfill(8)
+
             for j in range(4):
                 bits = byte[j:j + 2]
                 llc = LLC(dsap=0x69, ssap=(0b11011000 | int(bits, 2)))
@@ -44,24 +46,32 @@ class MyCovertChannel(CovertChannelBase):
         -
         -
         """
+        byte = ""
+        counter = 0
+
         def recv_packet(packet):
-            if packet.haslayer(LLC):
-                if packet[LLC].dsap == 0xff:
-                    raise Exception
+            nonlocal byte
+            nonlocal counter
+
+            if LLC in packet:
+                byte = str(bin(byte))[2:] + byte
+                counter += 1
+                if counter == 4:
+                    counter = 0
+                    byte = int(byte, 2)
+                    byte ^= 0x69
+                    received_bits = str(bin(byte))[2:].zfill(8)
+                    received_char = chr(int(received_bits, 2))
+                    print(received_char)
+                    self.message += received_char
+                    byte = ""
                 
-                packet.show()
-                self.message = self.message + str(packet[LLC].ssap)
+                    if received_char == '.':
+                        raise Exception("Dot character received in the message")
 
         try:
             sniff(prn=recv_packet)
-        except Exception:
-            pass
+        except Exception as e:
+            print(e)
 
-        chars = [self.message[i:i+8] for i in range(0, len(self.message), 8)]
-        
-        message = ""
-        for char in chars:
-            char ^= 0x69
-            message = message + self.convert_eight_bits_to_character(char)
-
-        self.log_message(message, log_file_name)
+        self.log_message(self.message, log_file_name)
