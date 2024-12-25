2e01fdf7764ff48c68aed81e97bf67c3ef57fa1d
Barış Berk Kottaş <brkottas1104@gmail.com>
2024-12-25 18:29:27 +0300

    fully implemented

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -19,8 +19,11 @@ class MyCovertChannel(CovertChannelBase):
 
     def send(self, log_file_name, parameter1, parameter2):
         """
-        -
-        -
+        - We first get the MAC address of the sender.
+        - Then, we create an Ethernet frame with the source MAC address of the sender and the destination MAC address of the broadcast address.
+        - We generate a random binary message.
+        - For each byte in the message, we XOR it with some number before sending it.
+        - We send the message in the last 2 bits of the SSAP field.
         """
         sender_mac = get_if_hwaddr("eth0")
         ether = Ether(src = sender_mac, dst = "ff:ff:ff:ff:ff:ff")
@@ -33,18 +36,22 @@ class MyCovertChannel(CovertChannelBase):
             byte ^= 0x69
             byte = str(bin(byte))[2:].zfill(8)
 
-            for j in range(4):
+            for j in range(0, 8, 2):
                 bits = byte[j:j + 2]
-                llc = LLC(dsap=0x69, ssap=(0b11011000 | int(bits, 2)))
+                llc = LLC(dsap=0x69, ssap=int(bits, 2))
                 packet = ether / llc
                 super().send(packet)
-            
+
                 
         
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
-        -
-        -
+        - We sniff the packets.
+        - For each packet, we check if it has LLC layer.
+        - If it has, we extract the last 2 bits of the SSAP field.
+        - When we receive the whole 8 bits, we XOR them with some number and convert them to a character.
+        - We check if the received character is a dot character. If it is, we stop receiving the message.
+        - We log the message to the log file.
         """
         byte = ""
         counter = 0
@@ -54,18 +61,19 @@ class MyCovertChannel(CovertChannelBase):
             nonlocal counter
 
             if LLC in packet:
-                byte = str(bin(byte))[2:] + byte
+                byte = byte + bin(packet[LLC].ssap)[2:].zfill(8)[-2:]
                 counter += 1
+                
                 if counter == 4:
                     counter = 0
                     byte = int(byte, 2)
                     byte ^= 0x69
-                    received_bits = str(bin(byte))[2:].zfill(8)
-                    received_char = chr(int(received_bits, 2))
-                    print(received_char)
+
+                    received_char = chr(byte)
+                    
                     self.message += received_char
                     byte = ""
-                
+
                     if received_char == '.':
                         raise Exception("Dot character received in the message")
 
